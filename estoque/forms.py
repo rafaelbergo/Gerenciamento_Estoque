590fa2f70b528c60bf746edcba429f09c78f2ed3
Rafael Bergo <rafaelbergo503@gmail.com>
2024-04-16 16:35:48 -0300

    corrigido campos de criar-venda

--- estoque/forms.py
+++ estoque/forms.py
@@ -23,16 +23,16 @@ class BuscarClienteForm(forms.Form):
 class VendaForm(forms.Form):
     # Dados do cliente
     cpf = forms.CharField(label='CPF do Cliente', max_length=14, widget=forms.TextInput(attrs={'placeholder': 'XXX.XXX.XXX-XX', 'pattern': '[0-9]{11}', 'title': 'Digite apenas números'}))
-    nome_cliente = forms.CharField(label='Nome do Cliente', required=False, widget=forms.TextInput(attrs={'readonly': True}))
-    endereco = forms.CharField(label='Endereço', required=False, widget=forms.TextInput(attrs={'readonly': True}))
-    cep = forms.CharField(label='CEP', required=False, widget=forms.TextInput(attrs={'readonly': True}))
+    nome_cliente = forms.CharField(label='Nome do Cliente', required=False, disabled=True, widget=forms.TextInput(attrs={'readonly': True}))
+    endereco = forms.CharField(label='Endereço', required=False, disabled=True, widget=forms.TextInput(attrs={'readonly': True}))
+    cep = forms.CharField(label='CEP', required=False, disabled=True, widget=forms.TextInput(attrs={'readonly': True}))
     buscar_cliente = forms.CharField(label='', widget=forms.HiddenInput(), required=False)
 
     # Dados dos produtos
     produto_id = forms.IntegerField(label='ID do Produto', required=False)
-    produto_nome = forms.CharField(label='Nome do Produto', required=False, widget=forms.TextInput(attrs={'readonly': True}))
-    produto_preco = forms.DecimalField(label='Preço do Produto', required=False, widget=forms.TextInput(attrs={'readonly': True}))
-    produto_estoque = forms.IntegerField(label='Quantidade em Estoque', required=False, widget=forms.TextInput(attrs={'readonly': True}))
+    produto_nome = forms.CharField(label='Nome do Produto', required=False, disabled=True, widget=forms.TextInput(attrs={'readonly': True}))
+    produto_preco = forms.DecimalField(label='Preço do Produto', required=False, disabled=True, widget=forms.TextInput(attrs={'readonly': True}))
+    produto_estoque = forms.IntegerField(label='Quantidade em Estoque', required=False, disabled=True, widget=forms.TextInput(attrs={'readonly': True}))
     
     # Dados da venda
     data = forms.DateField(label='Data', widget=forms.DateInput(attrs={'type': 'date'}), initial=datetime.date.today)
