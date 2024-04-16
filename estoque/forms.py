105631abe4752d407976f831dd7bffa83a7b94ab
Rafael Bergo <rafaelbergo503@gmail.com>
2024-04-15 23:02:42 -0300

    funcionando

--- estoque/forms.py
+++ estoque/forms.py
@@ -2,9 +2,20 @@ import datetime
 from django import forms
 
 class VendaForm(forms.Form):
+    # Dados do cliente
     cpf = forms.CharField(label='CPF do Cliente', max_length=14, widget=forms.TextInput(attrs={'placeholder': 'XXX.XXX.XXX-XX', 'pattern': '[0-9]{11}', 'title': 'Digite apenas números'}))
+    nome_cliente = forms.CharField(label='Nome do Cliente', required=False, widget=forms.TextInput(attrs={'readonly': True}))
+    endereco = forms.CharField(label='Endereço', required=False, widget=forms.TextInput(attrs={'readonly': True}))
+    cep = forms.CharField(label='CEP', required=False, widget=forms.TextInput(attrs={'readonly': True}))
+
+    
+
+    # Dados da venda
     data = forms.DateField(label='Data', widget=forms.DateInput(attrs={'type': 'date'}), initial=datetime.date.today)
     valor = forms.DecimalField(label='Valor', min_value=0, decimal_places=2)
+
+
+
     #forma_pagamento = forms.ChoiceField(label='Forma de Pagamento', choices=OPCOES_PAGAMENTO)
     #produtos = forms.ModelMultipleChoiceField(label='Produtos', queryset=Produto.objects.all())
     #quantidades = forms.IntegerField(label='Quantidades', min_value=1)
